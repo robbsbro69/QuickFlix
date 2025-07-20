7ead2c0d9d9d210638367a5106a9be30a6b0cc51
robbsbro69 <robbsbro369@proton.me>
2025-07-20 16:04:02 +0545

    add controller and models

---
+++ server/controllers/showController.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+
+
+export const getNowPlayingMovies = async (req,res) => {
+    try {
+        const { data } = await axios.get('https://api.themoviedb.org/3/movie/now_playing', {
+            headers: {
+                Authorization: `Bearer ${process.env.TMDB_API_KEY}`
+            }
+        })
+
+        const movies = data.results;
+        res.json(
+            {
+                sucess: true,
+                movies: movies
+            })
+    } catch (error) {
+        console.log(error);
+        res.json(
+            {
+                sucess: false, 
+                message: error.message
+            })
+    }
+}
--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ import connectDB from './configs/db.js';
 import { clerkMiddleware } from '@clerk/express'
 import { serve } from "inngest/express";
 import { functions, inngest } from './inngest/index.js';
+import showRouter from './routes/showRoutes.js';
 
 const app = express();
 const port = 3000;
@@ -19,5 +20,6 @@ app.use(clerkMiddleware())
 // API Routes
 app.get('/', (req,res) => res.send('Server is live!'))
 app.use('/api/inngest', serve({client: inngest, functions}))
+app.use('api/show', showRouter)
 
 app.listen(port, () => console.log(`Server listening at http://localhost:${port}`));
