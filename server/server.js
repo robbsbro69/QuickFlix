c274c1e3047cc1ecb46f523231556b651b3942ef
robbsbro69 <robbsbro369@proton.me>
2025-07-20 13:51:22 +0545

    implementing clerk and inngest

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ import cors from 'cors';
 import 'dotenv/config';
 import connectDB from './configs/db.js';
 import { clerkMiddleware } from '@clerk/express'
+import { serve } from "inngest/express";
+import { functions, inngest } from './inngest/index.js';
 
 const app = express();
 const port = 3000;
@@ -16,5 +18,6 @@ app.use(clerkMiddleware())
 
 // API Routes
 app.get('/', (req,res) => res.send('Server is live!'))
+app.use('/api/inngest', serve({client: inngest, functions}))
 
 app.listen(port, () => console.log(`Server listening at http://localhost:${port}`));
